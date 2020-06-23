35ba140a077ac2b7ab0668c7f46fc8f67831f44c
Bruno Alves <alves.bruno945@gmail.com>
2020-06-23 13:17:36 -0300

    Grade Avarage
    Calculates average

---
+++ Minha Nota Anhembi/ViewController.swift
@@ -0,0 +1,52 @@
+//
+//  ViewController.swift
+//  Minha Nota Anhembi
+//
+//  Created by Bruno Alves da Silva on 18/06/20.
+//  Copyright © 2020 Bruno Alves da Silva. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var txtN2: UITextField!
+    @IBOutlet weak var txtN1: UITextField!
+    var resultado: Double = 0
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        view.endEditing(true)
+    }
+    
+    @IBAction func btnCalcular(_ sender: Any) {
+        if let mediaN1 = Double(txtN1.text!), let mediaN2 = Double(txtN2.text!) {
+            resultado = (mediaN1 * 0.4) + (mediaN2 * 0.6)
+            performSegue(withIdentifier: "seguePrimeiraTela", sender: nil)
+        }
+    }
+    
+    func showResults() {
+        if resultado >= 6.0 {
+            performSegue(withIdentifier: "seguePrimeiraTela", sender: nil)
+        } else {
+            performSegue(withIdentifier: "seguePrimeiraTela", sender: nil)
+        }
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if let destination = segue.destination as? primeiraTela {
+            if resultado >= 6.0 {
+                destination.result = String(Int(resultado))
+                destination.resultSituacao = "Aprovado"
+            } else {
+                destination.result = String(Int(resultado))
+                destination.resultSituacao = "Reprovado"
+            }
+        }
+    }
+}
+
---
+++ Minha Nota Anhembi/primeiraTela.swift
@@ -0,0 +1,35 @@
+//
+//  primeiraTela.swift
+//  Minha Nota Anhembi
+//
+//  Created by Bruno Alves da Silva on 21/06/20.
+//  Copyright © 2020 Bruno Alves da Silva. All rights reserved.
+//
+
+import UIKit
+
+class primeiraTela: UIViewController {
+
+    @IBOutlet weak var txtSituacao: UILabel!
+    @IBOutlet weak var txtResultado: UILabel!
+    var result: String?
+    var resultSituacao: String?
+    @IBOutlet weak var image: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        if let nameToDisplay = result, let nameToDisplayS = resultSituacao {
+            txtResultado.text = nameToDisplay
+            txtSituacao.text = nameToDisplayS
+            
+            if nameToDisplayS == "Aprovado" {
+                self.view.backgroundColor = UIColor.init(red: 0.51, green: 255, blue: 102, alpha: 1.00)
+                image.image = UIImage(named: "smile")
+            } else {
+                self.view.backgroundColor = UIColor.init(red: 255.0, green: 0.00, blue: 0.00, alpha: 1.00)
+                image.image = UIImage(named: "sad")
+            }
+        }
+    }
+}
